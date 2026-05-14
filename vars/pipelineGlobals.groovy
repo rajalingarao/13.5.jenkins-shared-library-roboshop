e8377b1915829e3ff2a801d265cc99983a9f405c
rajalingarao4545 <rajalingarao4545@gmail.com>
2026-05-14 19:10:13 +0530

    jenkins shared library roboshop

---
+++ vars/pipelineGlobals.groovy
@@ -0,0 +1,16 @@
+def getAccountID(String environment){
+    switch(environment) { 
+        case 'dev': 
+            return "805778285734"
+        case 'qa':
+            return "805778285734"
+        case 'uat':
+            return "805778285734"
+        case 'pre-prod':
+            return "805778285734"
+        case 'prod':
+            return "805778285734"
+        default:
+            return "nothing"
+    } 
+}

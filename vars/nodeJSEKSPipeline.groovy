5921263557b72a2c737b4a43264e49fc5e856ed3
rajalingarao4545 <rajalingarao4545@gmail.com>
2026-05-17 15:50:35 +0530

    jenkins shared library roboshop

--- vars/nodeJSEKSPipeline.groovy
+++ vars/nodeJSEKSPipeline.groovy
@@ -58,7 +58,11 @@ def call(Map configMap){
             stage('Docker build') {
                 
                 steps {
-                    withAWS(region: 'us-east-1', credentials: "aws-creds-${environment}") {
+                    // withAWS(region: 'us-east-1', credentials: "aws-creds-${environment}") {
+                       
+                withCredentials([[$class: 'AmazonWebServicesCredentialsBinding',
+      credentialsId: 'aws-creds']]) {
+                       
                         sh """
                         aws ecr get-login-password --region ${region} | docker login --username AWS --password-stdin ${account_id}.dkr.ecr.us-east-1.amazonaws.com
 
